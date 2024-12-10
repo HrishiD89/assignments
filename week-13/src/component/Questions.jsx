f42a56c4eaffc2ca709d583feffcf02ea3ab12de
Hrishikesh Kalita <95520030+HrishiD89@users.noreply.github.com>
2024-12-10 19:44:19 +0530

    updated the sm screen sidebar

--- week-13/src/component/Questions.jsx
+++ week-13/src/component/Questions.jsx
@@ -5,7 +5,7 @@ import CheckImg from "../assets/check.svg";
 const Questions = ({ questions, filterData }) => {
   return (
     <>
-      <div className="w-full h-[550px] flex flex-col mt-6 overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-800">
+      <div className="w-full h-[550px] flex flex-col mt-6 overflow-y-scroll">
         {questions.length == 0 ? (
           <div className="bg-secondaryColor flex items-center justify-center text-gray-400 w-full sm:items-center gap-2 px-4 py-2 sm:p-2 text-sm rounded-md">
             <p>no question found</p>
@@ -20,9 +20,9 @@ const Questions = ({ questions, filterData }) => {
                     index % 2 === 0 ? "bg-secondaryColor" : ""
                   } rounded-md `}
                 >
-                  <div className="flex items-start sm:items-center gap-2 px-4 py-2 sm:p-2 text-sm ">
+                  <div className="flex items-start sm:items-center gap-2 px-4 py-2 md:py-3 sm:p-2 text-sm ">
                     <span className="sm:px-4">
-                      <img src={CheckImg} alt="check-img" className="w-3 h-3 mt-2 sm:mt-0" />
+                      <img src={CheckImg} alt="check-img" className="w-3 h-3 mt-1 sm:mt-0" />
                     </span>
                     <span>{q.q_id}</span>
                     <p className="flex w-full  gap-3 justify-between pr-3 items-center lg:pr-3 xl:pr-16 sm:py-1">
